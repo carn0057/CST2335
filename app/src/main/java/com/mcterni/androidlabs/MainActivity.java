fbf7ade6a252df13c7695ea871345825fd6758f3
Mateus Terni <mateus@absotware.com.br>
2019-09-18 11:12:06 -0400

    Added 3 layouts for Lab 2

--- app/src/main/java/com/mcterni/androidlabs/MainActivity.java
+++ app/src/main/java/com/mcterni/androidlabs/MainActivity.java
@@ -1,14 +1,16 @@
 package com.mcterni.androidlabs;
 
-import androidx.appcompat.app.AppCompatActivity;
-
 import android.os.Bundle;
+
+import androidx.appcompat.app.AppCompatActivity;
 
 public class MainActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main_linear);
+        //setContentView(R.layout.activity_main_grid);
+        //setContentView(R.layout.activity_main_relative);
     }
 }
